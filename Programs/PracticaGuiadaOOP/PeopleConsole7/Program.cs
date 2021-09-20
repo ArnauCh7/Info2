a87c3ab7370b7b53b9fa2a377013baaaae7adb9a
= <=>
2021-09-20 18:55:08 +0200

    deberes2ndodia

--- Programs/PracticaGuiadaOOP/PeopleConsole7/Program.cs
+++ Programs/PracticaGuiadaOOP/PeopleConsole7/Program.cs
@@ -11,7 +11,65 @@ namespace PeopleConsole7
     {
         static void Main(string[] args)
         {
-            
+            // Create an empty list and three people
+            CList myList = new CList();
+            CPerson p1 = new CPerson();
+            CPerson p2 = new CPerson();
+            CPerson p3;
+            try
+            {
+                //Asks the user to enter the parameters of the two first CPersons
+                Console.WriteLine("Write the name of the person: ");
+                p1.SetName(Console.ReadLine());
+
+                Console.WriteLine("Enter the person's age: ");
+                p1.SetAge(Convert.ToInt32(Console.ReadLine()));
+
+                Console.WriteLine("Enter person's height: ");
+                p1.SetHeight(float.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Write the name of the person: ");
+                p2.SetName(Console.ReadLine());
+
+                Console.WriteLine("Enter the person's age: ");
+                p2.SetAge(Convert.ToInt32(Console.ReadLine()));
+
+                Console.WriteLine("Enter person's height: ");
+                p2.SetHeight(float.Parse(Console.ReadLine()));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("There is a format error, please try again");
+            }
+            // Add two people to the list
+            myList.AddPerson(p1);
+            myList.AddPerson(p2);
+            // Get the first person in the list
+            p3 = myList.GetPerson(0);
+            if (p3 != null)
+            {
+                Console.WriteLine("The first person is " + p3.GetName());
+            }
+            else
+            {
+                Console.WriteLine("There is nobody in the list");
+            }
+
+            // Get the number of people in the list
+            int n = myList.GetNumber();
+            Console.WriteLine("The are {0} people in the list ", n);
+            // Get the youngest person in the list
+            CPerson p4 = myList.GetYoungestPerson();
+            if (p4 != null)
+            {
+                Console.WriteLine("The youngest person is " + p4.GetName());
+            }
+            else
+            {
+                Console.WriteLine("There is nobody in the list");
+            }
+
+            Console.ReadKey();
         }
     }
 }
--- Programs/PracticaGuiadaOOP/PeopleLib/CList.cs
+++ Programs/PracticaGuiadaOOP/PeopleLib/CList.cs
@@ -1,12 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 namespace PeopleLib
 {
-    class CList
+    public class CList
     {
+        const int MAXP = 10; // maximum number of people
+        int number; // number of people in the list
+        CPerson[] people; // vector of people
+        /// Constructor. Initializes the attributes of the list
+        public CList()
+        {
+            number = 0;
+            people = new CPerson[MAXP];
+        }
+        /// Adds a person at the end of the list
+        /// param p: The person to be added
+        /// returns: 0 everything Ok, -1 the vector was full
+        public int AddPerson(CPerson p)
+        {
+            if (number == MAXP)
+            {
+                return -1;
+            }
+            else
+            {
+                people[number] = p;
+                number++;
+                return 0;
+            }
+        }
+
+        /// Returns the person in a particular position of the list
+        /// param i: Position in the list
+        /// returns: Person at "i", null if index not valid
+        public CPerson GetPerson(int i)
+        {
+            if (i < 0 || i >= number)
+            {
+                return null;
+            }
+            else
+            {
+                return people[i];
+            }
+        }
+
+        public int GetNumber()
+        {
+            return number;
+        }
+        public CPerson GetYoungestPerson()
+        {
+            if (number == 0)
+            {
+                return null;
+            }
+            else
+            {
+                CPerson youngest = people[0];
+                for (int i = 1; i < number; i++)
+                {
+                    if (youngest.IsOlderThan(people[i]))
+                    {
+                        youngest = people[i];
+                    }
+                }
+                return youngest;
+            }
+        }
+
     }
 }
