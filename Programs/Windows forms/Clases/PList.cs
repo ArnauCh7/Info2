7415a5b0102e0f2f6c7dfa1f04287f54042a3424
= <=>
2021-09-27 10:57:28 +0200

    windows forms

---
+++ Programs/Windows forms/Clases/PList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class PList
+    {
+        const int Max = 100;
+        Punto[] lista;
+        int num;
+
+        public PList()
+        {
+            num = 0;
+            lista = new Punto[Max];
+        }
+        public void AddPunto(Punto p)
+        {
+            try
+            {
+                if (num != 100)
+                {
+                    lista[this.num] = p;
+                    this.num++;
+                }
+                else
+                {
+                    Console.WriteLine("The list is already full");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format ERROR!");
+            }
+        }
+        public Punto GetPunto(int i)
+        {
+            if (num != 0)
+            {
+                return this.lista[i];
+            }
+            else
+            {
+                Console.WriteLine("The list is empty");
+                return null;
+            }
+        }
+        public void MoveAll(int incX, int incY)
+        {
+            for(int i = 0; i<num; i++)
+            {
+                lista[i].Move(incX, incY);
+            }
+        }
+    }
+}
---
+++ Programs/Windows forms/Clases/Punto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class Punto
+    {
+        int X;
+        int Y;
+
+        public Punto(int X, int Y)
+        {
+            try
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format ERROR!");
+            }
+        }
+
+        public int GetX()
+        {
+            return this.X;
+        }
+
+        public int GetY()
+        {
+            return this.Y;
+        }
+
+        public void Move(int incX, int incY)
+        {
+            this.X = this.X + incX;
+            this.Y = this.Y + incY;
+        }
+    }
+}
